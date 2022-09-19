c655327dbaa2ff278f7a61fa7d8e979ad5428f8c
GiovannyCC <giovannyjchoquecenteno@gmail.com>
2022-09-18 21:08:50 -0500

    Creacion de repositorio e instalacion de daisyui2}

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        Base proyecto Fundacion
+        <button className='btn btn-primary'>Aceptar</button>
+    </div>
+  )
+}
+
+export default App
