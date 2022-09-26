6c53431399449f608b699a348f31389fb1d7f295
Eduardo <jorge.ecrg@gmail.com>
2022-09-26 11:07:39 -0400

    View Categories for Admin add mooks

--- src/pages/admin/Categoria/Categoria.jsx
+++ src/pages/admin/Categoria/Categoria.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Modal from '../../../components/elements/Modal'
 import { remove } from '../../../utils/messages'
+import { categorias } from '../../../utils/mocks/categoria'
 
 const Categoria = () => {
 
@@ -34,48 +35,47 @@ const Categoria = () => {
           </thead>
 
           <tbody>
-
-            <tr>
-              <td>
-                title
-              </td>
-              <td>
-                <Modal title={"Editar"} openModalText={"editar"} modalId={"modalEdit"} btnStyle={"btn-prim"}>
-                  <form className='my-5'>
-                    <div>
-                      <label>Nombre</label>
-                      <input type="text" placeholder="name..." className="input input-bordered w-full" />
-                    </div>
-                    <div className='mt-4'>
-                      <a href="#" className='btn-custom btn-prim no-underline'>
-                        actualizar
-                      </a>
-                    </div>
-                  </form>
-                </Modal>
-              </td>
-              <td>
-                <Modal title={"Eliminar"} openModalText={"eliminar"} modalId={"modalDelete"} btnStyle={"btn-dang"}>
-                  <div className='mt-4'>
-                    <div className={"my-4"}>
-                      <h3>{remove}</h3>
-                    </div>
-                    <div>
-                      <a href="#" className='btn-custom btn-prim no-underline'>
-                        confirmar
-                      </a>
-                      <a href="#" className='btn-custom btn-dang no-underline ml-2'>
-                        cerrar
-                      </a>
-                    </div>
-                  </div>
-                </Modal>
-              </td>
-            </tr>
-
+            {
+              categorias.map((categoria) => (
+                <tr key={categoria.id.toString()}>
+                  <td>{categoria.nombre}</td>
+                  <td>
+                    <Modal title={"Editar"} openModalText={"editar"} modalId={"modalEdit"} btnStyle={"btn-prim"}>
+                      <form className='my-5'>
+                        <div>
+                          <label>Nombre</label>
+                          <input type="text" placeholder="name..." className="input input-bordered w-full" />
+                        </div>
+                        <div className='mt-4'>
+                          <a href="#" className='btn-custom btn-prim no-underline'>
+                            actualizar
+                          </a>
+                        </div>
+                      </form>
+                    </Modal>
+                  </td>
+                  <td>
+                    <Modal title={"Eliminar"} openModalText={"eliminar"} modalId={"modalDelete"} btnStyle={"btn-dang"}>
+                      <div className='mt-4'>
+                        <div className={"my-4"}>
+                          <h3>{remove}</h3>
+                        </div>
+                        <div>
+                          <a href="#" className='btn-custom btn-prim no-underline'>
+                            confirmar
+                          </a>
+                          <a href="#" className='btn-custom btn-dang no-underline ml-2'>
+                            cerrar
+                          </a>
+                        </div>
+                      </div>
+                    </Modal>
+                  </td>
+                </tr>
+              ))
+            }
           </tbody>
         </table>
-
       </div>
 
     </div>
