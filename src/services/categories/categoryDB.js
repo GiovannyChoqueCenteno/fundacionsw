20d260a59e5aa17986bbe38301d04ac86011fbf9
ttimbal <timbal.tmb@gmail.com>
2022-10-01 16:42:37 -0400

    Implement a CRUD for categories

---
+++ src/services/categories/categoryDB.js
@@ -0,0 +1,62 @@
+import {addDoc, collection, doc, getDoc, getDocs, onSnapshot, query, setDoc} from "firebase/firestore";
+import {firestore} from "../../config/firebase.js";
+
+async function saveCategory(data) {
+    await addDoc(collection(firestore, "categories"), data);
+}
+
+async function updateCategory(id,data) {
+    await setDoc(doc(firestore, "categories"), data);
+}
+
+async function getCategory(id){
+    const docRef = doc(firestore, "categories", id);
+    const document = await getDoc(docRef);
+    return {
+        id: document.id,
+        ...document.data()
+    }
+}
+
+async function getAllCategories(){
+    const querySnapshot = await getDocs(collection(firestore, "categories"));
+    const list=[];
+    querySnapshot.forEach((currentDoc) => {
+        document={
+            id: currentDoc.id,
+            ...currentDoc.data()
+        }
+        list.push(document);
+    });
+
+    return list;
+}
+
+/**
+ * @param setCategories is a hook to change value every time that categories changing
+ * @returns a function to unsubscribe to listen categories "unsubscribe()"
+ */
+async function listenAllCategories(setCategories){
+    const q = query(collection(firestore, "categories"));
+    return onSnapshot(q, (querySnapshot) => {
+        const list = [];
+        querySnapshot.forEach((currentDoc) => {
+            document = {
+                id: currentDoc.id,
+                ...currentDoc.data()
+            }
+            list.push(document);
+        });
+
+        setCategories(list);
+    });
+}
+
+
+export {
+    saveCategory,
+    updateCategory,
+    getCategory,
+    getAllCategories,
+    listenAllCategories
+}
