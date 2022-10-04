e7104cef25d3e2ee1109302bcabf6ac37419b3f6
GiovannyCC <giovannyjchoquecenteno@gmail.com>
2022-10-03 20:08:11 -0500

    Añadiendo todas las partes

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,20 +4,12 @@ import { useState } from "react";
 const useForm = (initialState = {}) => {
     const [form, setForm] = useState(initialState);
 
-<<<<<<< HEAD
-    const handleChange = (e)=>{
-        
-        setForm(form => (
-            {
-                ...form,
-                [e.target.name] :  e.target.value
-=======
     const handleChange = (e) => {
         setForm(form => (
             {
                 ...form,
                 [e.target.name]: e.target.value
->>>>>>> ef01f10bddab73bd517ea48fedb0b7828233e226
+
             }
         ))
     }
