4743b2c3882352e324cf942d3462060f48dae5b9
JhonatanWCL99 <wilsoncoyoleon@gmail.com>
2022-11-02 21:41:54 -0400

    last

--- src/components/elements/Header.jsx
+++ src/components/elements/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Menu from "./Menu.jsx";
 
-function Header({menuItems, authAction}) {
+function Header({ menuItems, authAction }) {
     return (
         <header className={'bg-white h-16 flex items-center p-5'}>
             <h1 className={'flex-none text-3xl text-theme-primary font-bold'}>{'Fundaciones'}</h1>
             <div className="grow">
-                <Menu items={menuItems}/>
+                <Menu items={menuItems} />
             </div>
             <div className="flex-none">
                 {authAction}
